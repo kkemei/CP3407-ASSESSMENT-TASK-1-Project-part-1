b115ea6983f6e6a2dee14319229d418d2f5f949d
kkemei <kipngetich.kemei@my.jcu.edu.au>
2018-10-28 13:57:46 +1000

    added download functonality

---
+++ Wilderness Weather Station/Wilderness Weather Station/Excel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Wilderness_Weather_Station
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "";
+        }
+
+        public void WriteToExcel(int i, int j, string s)
+        {
+            i++;
+            j++;
+            ws.Cells[i, j].Value2 = s;
+        }
+
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        public void SaveAs(String path)
+        {
+            wb.SaveAs(path);
+        }
+
+        public void Close()
+        {
+            wb.Close();
+        }
+    }
+}
--- Wilderness Weather Station/Wilderness Weather Station/Station1.cs
+++ Wilderness Weather Station/Wilderness Weather Station/Station1.cs
@@ -12,13 +12,64 @@ namespace Wilderness_Weather_Station
     {
         const string APPID = "8537c81f4e28567a004122c1b81937d6";
         //string cityName = "Townsville";
-
+        string CityName;
+        string Country;
+        Double Temp;
+        Double Long;
+        Double Lat;
+        Double WindSpeed;
+        Double WindDir;
+        Double Pressure;
+        Double Humidity;
 
         public Home(string station)
         {
-            string cityName = station;
+            //string cityName = station;
             InitializeComponent();
             getWeather(station);
+        }
+
+
+        public void WriteData()
+        {
+            //FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            //if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    string selectedFolder = folderBrowserDialog.SelectedPath;
+
+                //MessageBox.Show(selectedFolder);
+
+            string curDate = DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-tt");
+
+            Excel excel = new Excel(@"template.xlsx", 1);
+
+            //excel.SaveAs(selectedFolder);
+            excel.WriteToExcel(0, 0, "City");
+            excel.WriteToExcel(0, 1, "Country");
+            excel.WriteToExcel(0, 2, "Temperature (\u00B0C)");
+            excel.WriteToExcel(0, 3, "Longitude (\u00B0)");
+            excel.WriteToExcel(0, 4, "Latitude(\u00B0)");
+            excel.WriteToExcel(0, 5, "Wind Speed (KM/H)");
+            excel.WriteToExcel(0, 6, "Wind Direction (\u00B0)");
+            excel.WriteToExcel(0, 7, "Pressure (Pa)");
+            excel.WriteToExcel(0, 8, "Humidity (%)");
+
+            excel.WriteToExcel(1, 0, CityName);
+            excel.WriteToExcel(1, 1, Country);
+            excel.WriteToExcel(1, 2, string.Format("{0}", Temp));
+            excel.WriteToExcel(1, 3, string.Format("{0}", Long));
+            excel.WriteToExcel(1, 4, string.Format("{0}", Lat));
+            excel.WriteToExcel(1, 5, string.Format("{0}", WindSpeed));
+            excel.WriteToExcel(1, 6, string.Format("{0}", WindDir));
+            excel.WriteToExcel(1, 7, string.Format("{0}", Pressure));
+            excel.WriteToExcel(1, 8, string.Format("{0}", Humidity));
+
+            //excel.Save();
+            excel.SaveAs(string.Format(@"{0} - {1}.xlsx", CityName, curDate));
+
+            MessageBox.Show(string.Format("Saved To Documents"));
+            excel.Close();
         }
 
         void getWeather(string station)
@@ -45,16 +96,18 @@ namespace Wilderness_Weather_Station
                 lblPressure.Text = string.Format("{0} Pa", outPut.main.pressure);
                 lblHumidity.Text = string.Format("{0} %", outPut.main.humidity);
                 label4.Text = string.Format("{0} Weather Station", outPut.name);
+
+                CityName = outPut.name;
+                Country = outPut.sys.country;
+                Temp = outPut.main.temp;
+                Long = outPut.coord.lon;
+                Lat = outPut.coord.lat;
+                WindSpeed = outPut.wind.speed;
+                WindDir = outPut.wind.deg;
+                Pressure = outPut.main.pressure;
+                Humidity = outPut.main.humidity;
             }
         }
-
-        private void WriteToExcel(int i, int j, string s)
-        {
-            i++;
-            j++;
-            WindowState.Cells[i, j].Vlaue2 = s;
-        }
-
 
         private void btnBack_Click(object sender, EventArgs e)
         {
@@ -66,7 +119,7 @@ namespace Wilderness_Weather_Station
 
         private void btnDownload_Click(object sender, EventArgs e)
         {
-            
+            WriteData();
         }
     }
 }
